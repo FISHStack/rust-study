0f281445c373963de766ab493dd530c186832677
fish <gcslyp@gmail.com>
2019-12-11 13:52:04 +0800

    对现有代码fn添加注释

--- rusty_base/src/main.rs
+++ rusty_base/src/main.rs
@@ -13,20 +13,24 @@ fn main() {
     print_values(x1,y1);
 }
 
+//打印 i32类型value
 fn print_value(value: i32) {
     println!("The value given was: {}", value);
 }
 
+//打印多个 i32类型value
 fn print_values(value_1: i32, value_2: i32) {
     println!("Values given were: {} and {}", value_1, value_2);
 }
 
+//对参数进行加1运算，并打印运算后的结果，原参数值不变
 fn increase_by_one(value: i32) {
     let mut value2 = value;
     value2 += 1;
     print_value(value2)
 }
 
+//分别对两个传感进行整除以及除余， 并返回两个结果值
 fn divmod(x: i32, y: i32) -> (i32, i32) {
     return (x / y, x % y);
 }
