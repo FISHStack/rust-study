7497e63ae4291c5bd7041d4a546beefb7f9282d4
lan.liang <liangyuanpengem@163.com>
2020-06-30 13:42:32 +0800

    rust-by-example添加functions示例

---
+++ rust-by-example/functions/closures/inputFunctions.rs
@@ -0,0 +1,22 @@
+// Define a function which takes a generic `F` argument
+// bounded by `Fn`, and calls it
+fn call_me<F: Fn()>(f: F) {
+    f();
+}
+
+// Define a wrapper function satisfying the `Fn` bound
+fn function() {
+    println!("I'm a function!");
+}
+
+fn main() {
+
+    //可以将闭包作为参数 也可以将函数作为参数
+
+    // Define a closure satisfying the `Fn` bound
+    let closure = || println!("I'm a closure!");
+
+    call_me(closure);
+    call_me(function);
+}
+
