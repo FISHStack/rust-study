54fb2938478c421565486768b452d90f448d20da
fish <gcslyp@gmail.com>
2020-07-06 00:12:29 +0800

    添加异步代码,无法启动,删除编译产物

--- rust_advanced/target/debug/deps/rust_advanced-e4db9e92574fd9cf.d
+++
@@ -1,6 +0,0 @@
-/disk/note/rust-study/rust_advanced/target/debug/deps/rust_advanced-e4db9e92574fd9cf: src/main.rs src/channel.rs
-
-/disk/note/rust-study/rust_advanced/target/debug/deps/rust_advanced-e4db9e92574fd9cf.d: src/main.rs src/channel.rs
-
-src/main.rs:
-src/channel.rs:
